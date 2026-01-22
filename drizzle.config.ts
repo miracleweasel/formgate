6ad8aed09ff39a4cedd3f994dd4dd820eead7206
faffnir68 <magicpoup@hotmail.com>
2026-01-23 00:35:09 +0900

    chore: force drizzle to use DATABASE_URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,6 @@ export default {
   out: "./drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DIRECT_URL || process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
